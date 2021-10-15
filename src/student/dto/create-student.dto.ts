3f78738758b0c4aa03230a3820d6a53b3b10e845
luisfelipe <luis.felipeszsv@gmail.com>
2021-10-15 20:03:16 -0300

    creating student base routes

---
+++ src/student/dto/create-student.dto.ts
@@ -0,0 +1,7 @@
+export class CreateStudentDto {
+  id: string;
+  name: string;
+  cpf: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/student/student.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
+
+@Injectable()
+export class StudentService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(CreateStudentDto: CreateStudentDto) {
+    return await this.prisma.student.create({ data: CreateStudentDto });
+  }
+
+  findAll() {
+    return `This action returns all student`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} student`;
+  }
+
+  update(id: number, updateStudentDto: UpdateStudentDto) {
+    return `This action updates a #${id} student`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} student`;
+  }
+}
